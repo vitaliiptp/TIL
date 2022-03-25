c36e933f3453a87ae2f32c8e3e8519896163e8ce
vitaliiptp <vitalii.potapenko@gmail.com>
2022-03-25 18:21:08 +0100

    closures 3.0

--- closures/closures.js
+++ closures/closures.js
@@ -184,3 +184,227 @@
 
 // let a = !undefined;
 // console.log(Boolean(a))
+
+
+
+// function makeFunc() {
+//     let name = 'Mozilla';
+//     function displayName() {
+//         console.log(name);
+//     }
+//     // return displayName;
+//     displayName();
+// }
+// makeFunc()
+// var myFunc = makeFunc();
+// myFunc();
+
+//
+// function outerFunc() {
+//     let outerVar = 'I am outside!';
+//     function innerFunc() {
+//         console.log(outerVar); // => logs "I am outside!"
+//     }
+//     return innerFunc;
+// }
+// function exec() {
+//     const myInnerFunc = outerFunc();
+//     myInnerFunc();
+// }
+// exec();
+
+
+// let countEven = 0;
+// const items = [1, 5, 100, 10];
+// items.forEach(iterator = (number) => {
+// // items.forEach(function iterator(number) {
+//         if (number % 2 === 0) {
+//             countEven++;
+//         }
+// })
+// console.log(countEven);
+
+
+
+
+// function logPerson() {
+//     console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+// }
+
+// function bind(obj) {
+//     this.name = obj.name
+//     this.age = obj.age
+//     this.job = obj.job
+//
+//     return logPerson()
+// }
+
+
+// function bind(context, fn) {
+//     return function(...args) {
+//         fn.apply(context, args)
+//     }
+// }
+//
+// function logPerson() {
+//     console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+// }
+//
+// const person1 = {name: 'John', age: 22, job: 'Frontend'};
+// const person2 = {name: 'Sara', age: 20, job: 'Backend'};
+//
+//
+// bind(person1, logPerson)()
+// bind(person2, logPerson)()
+
+
+
+
+// function makeFunc() {
+//     let name = 'Mozilla';
+//     function displayName() {
+//         return name;
+//     }
+//     return displayName;
+// }
+//
+// console.log(makeFunc()())
+// let myFunc = makeFunc();
+//
+//
+// let someArray = [];
+// someArray.push(myFunc())
+// someArray.push(makeFunc()())
+//
+// console.log(someArray)
+
+
+
+
+// CHALLENGE 1
+function createFunction() {
+    return function() {
+        console.log("hello");
+    }
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const function1 = createFunction();
+// function1(); // => should console.log('hello');
+
+
+// CHALLENGE 2
+function createFunctionPrinter(input) {
+    return function(){
+        console.log(input);
+    }
+
+}
+
+// /*** Uncomment these to check your work! ***/
+// const printSample = createFunctionPrinter('sample');
+// printSample(); // => should console.log('sample');
+// const printHello = createFunctionPrinter('hello');
+// printHello(); // => should console.log('hello');
+
+
+
+
+// CHALLENGE 3
+function outer() {
+    let counter = 0; // this variable is outside incrementCounter's scope
+    function incrementCounter () {
+        counter ++;
+        console.log('counter', counter);
+    }
+    return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+// /*** Uncomment these to check your work! ***/
+// willCounter(); // counter 1
+// willCounter(); // counter 2
+// willCounter(); // counter 3
+
+// jasCounter(); // counter 0
+// willCounter(); // counter 4
+
+
+function addByX(x) {
+    return function incrementCounter(num) {
+        // console.log(num)
+        return num += x;
+    };
+
+    // function incrementCounter(num) {
+    //   num += x;
+    //   console.log(num)
+    //   return num += x;
+    // };
+    // return incrementCounter;
+
+}
+
+// /*** Uncomment these to check your work! ***/
+const addByTwo = addByX(2);
+// addByTwo(1); // => should return 3
+// addByTwo(2); // => should return 4
+// addByTwo(3); // => should return 5
+
+// const addByThree = addByX(3);
+// addByThree(1); // => should return 4
+// addByThree(2); // => should return 5
+
+// const addByFour = addByX(4);
+// addByFour(4); // => should return 8
+// addByFour(5); // => should return 9
+
+
+// CHALLENGE 4
+function once(func) {
+    let counter = 0;
+    let result;
+    return function callCallback(num) {
+        counter++;
+        return counter < 2 ? (result = func(num)) : result;
+    }
+}
+
+
+// /*** Uncomment these to check your work! ***/
+const onceFunc = once(addByTwo);
+// console.log(onceFunc(4));  // => should log 6
+// console.log(onceFunc(10));  // => should log 6
+// console.log(onceFunc(9001));  // => should log 6
+
+
+// CHALLENGE 5
+function after(count, func) {
+    let counter = 0;
+    let result;
+    return function callbackCounter() {
+        counter++;
+        return counter >= count ? result = func() : result;
+    }
+}
+
+// /*** Uncomment these to check your work! ***/
+const called = function() { console.log('hello') };
+const afterCalled = after(3, called);
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => nothing is printed
+// afterCalled(); // => 'hello' is printed
+// afterCalled(); // => 'hello' is printed
+
+
+
+// CHALLENGE 6
+function delay(func, wait) {
+    setTimeout(func, wait);
+}
